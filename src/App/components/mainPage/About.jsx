02f9a6cb0d343addf3f7df3a52c269f6b384bcd8
khanh nguyen <khanhjoi1@gmail.com>
2023-11-30 10:07:36 +0700

    code finish about components

--- src/App/components/mainPage/About.jsx
+++ src/App/components/mainPage/About.jsx
@@ -16,12 +16,20 @@ const About = () => {
           perennials and ornamental grasses to supply her studio.
         </div>
       </div>
-      <div className="mt-10 ">
+      <div className="mt-10 w-full py-10 md:flex md:justify-between">
         <div
           style={{backgroundImage: `url(https://preview.colorlib.com/theme/florist/img/about/about-video.jpg.webp)`}}
-          className="h-[30rem]  group w-full bg-cover bg-center no-bg-repeat rounded-lg flex shadow-lg justify-center items-end cursor-pointer group-hover:shadow-2xl"
+          className="h-[30rem] z-10 group w-full  md:w-3/5 bg-cover bg-center no-bg-repeat rounded-lg flex shadow-lg justify-center items-end cursor-pointer group-hover:shadow-2xl"
         >
           <IoIosPlay className="text-pink-700 bg-slate-300 text-5xl mb-44 rounded-3xl shadow-2xl group-hover:opacity-80 transition-all duration-150" />
+        </div>
+        <div className="w-full my-4 md:-mt-8 md:-ml-14 md:w-3/5 lg:w-4/5 md:mx-4 ">
+          <div className="w-full h-full sm:px-10 sm:py-10 md:px-24 md:py-20 shadow-2xl ">
+            <h3 className="text-xl font-medium text-pink-400 mt-4">SLOW FLOWERS’ FLORAL INSIGHTS</h3>
+            <h1 className="text-3xl font-serif mt-4">Dried flowers are having a renaissance</h1>
+            <p className="mt-4 font-thin text-xl">This awareness has been stimulated by sustainable sourcing practices and the desire on the part of North American flower growers to “extend the season” beyond the last frost.</p>
+            <button className="mt-4 px-8 py-2 bg-pink-500 rounded-3xl text-slate-100 font-semibold">CONTACT US</button>
+          </div>
         </div>
       </div>
     </div>
